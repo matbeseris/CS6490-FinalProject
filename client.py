ab062cefe3cfd8f5e6f91080e910f2e8e4c9c64e
evan <ehrouda@gmail.com>
2020-04-19 14:20:22 -0600

    Added TLS1.2 DHE-RSA w/ AES256

--- client.py
+++ client.py
@@ -1,20 +1,31 @@
 import socket
+import ssl
 
 serverBluetoothMAC = "00:C2:C6:6F:B6:15"
 port = 20
 
 bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-bsock.connect((serverBluetoothMAC, port))
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ssl_context.set_ciphers("DHE-RSA-AES256-GCM-SHA384")
+c = ssl_context.get_ciphers()
+ssl_bsock_wrapped = ssl_context.wrap_socket(bsock, server_hostname=serverBluetoothMAC)
+
+ssl_bsock_wrapped.connect((serverBluetoothMAC, port))
 print("connected...")
+
+if not ssl_bsock_wrapped:
+    print("error connecting to server")
+    exit(1)
 
 while 1:
     sendmsg = input("Message to send: ")
     if sendmsg == "q":
         break
-    bsock.send(bytes(sendmsg, "UTF-8"))
+    ssl_bsock_wrapped.send(bytes(sendmsg, "UTF-8"))
 
-    recvmsg = bsock.recv(2048)
+    recvmsg = ssl_bsock_wrapped.recv(2048)
     if recvmsg:
         print("Recieved msg: " + recvmsg.decode("UTF-8"))
 
-bsock.close()
+ssl_bsock_wrapped.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 
 # my bluetooth adapter MAC address
 bluetoothMAC = "00:C2:C6:6F:B6:15"
@@ -6,18 +7,31 @@ port = 20 # any port that is available
 
 bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ssl_context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")
+ssl_context.load_dh_params("dhparams.pem")
+ssl_context.set_ciphers("DHE-RSA-AES256-GCM-SHA384")
+
 bsock.bind((bluetoothMAC, port))
 bsock.listen(1)
+
+print("Waiting for connection")
 
 try:
     client, addr = bsock.accept()
     print("client connected")
+    
+    ssl_client_wrapped = ssl_context.wrap_socket(client, server_side=True)
+    print("client socket wrapped in SSL with ciphers: " + 
+        str(ssl_client_wrapped.cipher()))
+    
     while 1:
-        msg = client.recv(2048)
+        msg = ssl_client_wrapped.recv(2048)
         if msg:
             print("Recieved message: " + msg.decode("UTF-8"))
-            client.send(msg)
+            ssl_client_wrapped.send(msg)
 except:
     print("closing socket")
-    client.close()
+    if ssl_client_wrapped:
+        ssl_client_wrapped.close()
     bsock.close()
