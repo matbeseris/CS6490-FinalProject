94bb30177a4267f364b330b477accdf09656d190
evan <ehrouda@gmail.com>
2020-04-20 10:21:30 -0600

    More cipher implementation and organization

--- client.py
+++ client.py
@@ -1,30 +1,53 @@
 import socket
 import ssl
+from CipherSuites import *
 
 # Bluetooth server information
 serverBluetoothMAC = "00:C2:C6:6F:B6:15"
 port = 20
 
-bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#####
+# Create a Bluetooth socket and wrap in an SSL connection
+#####
+def connectBluetoothServerSSL(mac, port):
+    bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
-ssl_context = getSSLContext_client(DHERSA_AES256)
-ssl_bsock_wrapped = ssl_context.wrap_socket(bsock, server_hostname=serverBluetoothMAC)
+    ssl_context = getSSLContext_client()
+    ssl_bsock_wrapped = ssl_context.wrap_socket(bsock, server_hostname=serverBluetoothMAC)
 
-ssl_bsock_wrapped.connect((serverBluetoothMAC, port))
-print("connected...")
+    ssl_bsock_wrapped.connect((mac, port))
+    return ssl_bsock_wrapped
 
-if not ssl_bsock_wrapped:
-    print("error connecting to server")
-    exit(1)
+######
+# Send user input to server who will echo the message back
+######
+def echoServer(bsock):
+    while 1:
+        sendmsg = input("Message to send: ")
+        if sendmsg == "q":
+            break
+        bsock.send(bytes(sendmsg, "UTF-8"))
 
-while 1:
-    sendmsg = input("Message to send: ")
-    if sendmsg == "q":
-        break
-    ssl_bsock_wrapped.send(bytes(sendmsg, "UTF-8"))
+        recvmsg = bsock.recv(2048)
+        if recvmsg:
+            print("Recieved msg: " + recvmsg.decode("UTF-8"))
+    return
 
-    recvmsg = ssl_bsock_wrapped.recv(2048)
-    if recvmsg:
-        print("Recieved msg: " + recvmsg.decode("UTF-8"))
+######
+# Handle the Bluetooth connection
+######
+def connectionHandler(mac, port):
+    ssl_bsock = connectBluetoothServerSSL(mac, port)
+    
+    if not ssl_bsock:
+        print("error connecting to server")
+        return -1
+    print("Connected to Bluetooth server")
 
-ssl_bsock_wrapped.close()
+    echoServer(ssl_bsock)
+
+    ssl_bsock.close()
+    
+if __name__ == "__main__":
+    connectionHandler(serverBluetoothMAC, port)
+
--- server.py
+++ server.py
@@ -2,41 +2,56 @@ import socket
 import ssl
 from CipherSuites import *
 
-# recieve messages from the client
-def client_handler(ssl_sock):
-    msg = ssl_sock.recv(2048)
-    while msg:
-        print("Recieved message: " + msg.decode("UTF-8"))
-        ssl_client_wrapped.send(msg)
-        msg = ssl_sock.recv(2048)
-
-
 # my bluetooth adapter MAC address
 bluetoothMAC = "00:C2:C6:6F:B6:15"
 port = 20 # any port that is available
 
-bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-
-ssl_context = getSSLContext_server(DHERSA_AES256)
-
-bsock.bind((bluetoothMAC, port))
-bsock.listen(1)
-
-print("Waiting for connection")
-
-try:
-    client, addr = bsock.accept()
-    print("client connected")
-    
-    ssl_client_wrapped = ssl_context.wrap_socket(client, server_side=True)
-    print("client socket wrapped in SSL with ciphers: " + 
-        str(ssl_client_wrapped.cipher()))
-    
-    client_handler(ssl_client_wrapped)
-except:
-    print("closing socket")
-    if ssl_client_wrapped:
-        ssl_client_wrapped.shutdown(socket.SHUT_RDWR)
-        ssl_client_wrapped.close()
+######
+# Recieve messages from the client and send them right back
+######
+def echoServer(bsock):
+    msg = bsock.recv(2048)
+    while msg:
+        print("Recieved message: " + msg.decode("UTF-8"))
+        bsock.send(msg)
+        msg = bsock.recv(2048)
         
-    bsock.close()
+######
+# Handle the interaction with the client
+######
+def clientHandler(bsock):
+    echoServer(bsock)
+
+######
+# Start the bluetooth server socket and wait for a client to connect
+# Wrap the connection in SSL and handle it
+######
+def startServer(mac, port):
+    bsock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+    ssl_context = getSSLContext_server(CipherSuites.DHERSA_AES256)
+
+    bsock.bind((mac, port))
+    bsock.listen(1)
+
+    print("Waiting for connection")
+
+    try:
+        client, addr = bsock.accept()
+        print("client connected")
+    
+        ssl_client_wrapped = ssl_context.wrap_socket(client, server_side=True)
+        print("client socket wrapped in SSL with ciphers: " + 
+            str(ssl_client_wrapped.cipher()))
+    
+        clientHandler(ssl_client_wrapped)
+    except:
+        print("closing socket")
+        if ssl_client_wrapped:
+            ssl_client_wrapped.shutdown(socket.SHUT_RDWR)
+            ssl_client_wrapped.close()
+        
+        bsock.close()
+        
+if __name__ == "__main__":
+    startServer(bluetoothMAC, port)
