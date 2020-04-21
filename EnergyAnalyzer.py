be983f23c54af1b815097a33a835285e3b891bc4
Mathew Beseris <mathewbeseris@gmail.com>
2020-04-21 11:50:26 -0600

    Added printout for energy results, gitignore pyc files

--- EnergyAnalyzer.py
+++ EnergyAnalyzer.py
@@ -12,11 +12,23 @@ def main():
         exit(0)
         
     # Run each of the scripts
+    msOutput = []
     i = 1
     while(i < len(sys.argv)):
         os.system("sudo ./analyzeEnergy.sh " + sys.argv[i])
         # Filtered output is in energytime.txt
+        f = open("energyTime.txt")
+        str = f.read()
+        str = str.split(" ms/s")[0]
+        msOutput.append(str)
         i+=1
+    
+    
+    # Print energy rating of each program
+    i = 0
+    print("*** Result energy ratings (higher = more energy used) *** ")
+    while(i < len(msOutput)):
+        print(sys.argv[i+1] + ":\t " + msOutput[i])
     
 
 if __name__ == '__main__':
