533b9b2658e36809f983fb95f5b3a2f3c730ff1c
Mathew Beseris <mathewbeseris@gmail.com>
2020-04-21 11:43:18 -0600

    Added py script that runs all inputted files

--- EnergyAnalyzer.py
+++ EnergyAnalyzer.py
@@ -1,20 +1,23 @@
 # This file will run the various Bluetooth connection methods
 # and analyize their power consumption, comparing and printing results
+import sys
+import os
 from KeyExchanges import DHERSA, DHAPSK
 from CipherMethods import AES, ChaCha20
-import energyusage
 
 
-def recursive_fib(n):
-    if (n <= 2): return 1
-    else: return recursive_fib(n-1) + recursive_fib(n-2)
-    
 def main():
-    energyusage.evaluate(recursive_fib, 40, pdf=True)
+    if(len(sys.argv) == 1):
+        print("Please enter at least 1 command line argument of the python program to run.")
+        exit(0)
+        
+    # Run each of the scripts
+    i = 1
+    while(i < len(sys.argv)):
+        os.system("sudo ./analyzeEnergy.sh " + sys.argv[i])
+        # Filtered output is in energytime.txt
+        i+=1
     
-
-
-
 
 if __name__ == '__main__':
     main()
